dfefef161003c246b2510e72e20faaa922f8f27f
DaniSchreibing <dani.schreibing@live.nl>
2025-06-12 09:39:38 +0200

    Fixed firebase file error for cloud

--- auth/src/firebase.config.ts
+++ auth/src/firebase.config.ts
@@ -34,7 +34,11 @@ const app = initializeApp(firebaseConfig);
 
 const serviceAccount = require("../FirebaseService.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 module.exports = {
